77029c46f3b6c900e4100602489e7fad1addc595
hukaijia <190916516@qq.com>
2020-01-08 18:08:07 +0800

    提交python基础语法

--- pythonbasic.py
+++ pythonbasic.py
@@ -19,3 +19,34 @@ print(10 / 3);
 print(9 / 3);
 print(10 // 3);
 print(10 % 3);
+#字符编码
+print(ord('A'));
+print(chr(66));
+print(len('ABC'));
+print('Hello, {0}, 成绩提升了 {1:.1f}%'.format('小明', 17.125));
+print('中文'.encode('gb2312'));
+#%d	整数
+#%f	浮点数
+#%s	字符串
+#%x	十六进制整数
+
+#******************-----------------***********************
+#list,list是一种有序的集合，可以随时添加和删除其中的元素。
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates);
+print(len(classmates));
+print(classmates[0]);
+#如果要取最后一个元素，除了计算索引位置外，还可以用-1做索引，直接获取最后一个元素
+print(classmates[-1]);
+classmates.append("hukaijia");
+print(classmates);
+print(len(classmates));
+#插入数据
+classmates.insert(0, 'Jack');
+print(classmates);
+#删除数据
+classmates.pop();
+print(classmates);
+#tuple，tuple和list非常类似，但是tuple一旦初始化就不能修改，tuple的陷阱：当你定义一个tuple时，在定义的时候，tuple的元素就必须被确定下来
+t = (1, 2);
+print(t);
