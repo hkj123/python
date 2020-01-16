cca37ff89b23fa132efbb64073b5f6800d95993b
hukaijia <190916516@qq.com>
2020-01-16 10:11:18 +0800

    提交python函数语法 面相对象

---
+++ oopadvanced.py
@@ -0,0 +1,122 @@
+#__slots__  允许对Student实例添加name和age属性  Python允许在定义class的时候，定义一个特殊的__slots__变量，来限制该class实例能添加的属性：
+class Student(object):
+    __slots__ = ('name', 'age') # 用tuple定义允许绑定的属性名称
+s = Student() # 创建新的实例
+s.name = 'Michael' # 绑定属性'name'
+s.age = 25 # 绑定属性'age'
+
+#@property 直接把属性暴露出去，虽然写起来很简单，但是，没办法检查参数，导致可以把成绩随便改
+class Student1(object):
+
+    def get_score(self):
+        return self._score
+
+    def set_score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+s = Student1()
+s.set_score(60) # ok!
+print(s.get_score())
+#Python内置的@property装饰器就是负责把一个方法变成属性调用的：
+class Student2(object):
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+#@property的实现比较复杂，我们先考察如何使用。把一个getter方法变成属性，只需要加上@property就可以了，此时，@property本身又创建了另一个装饰器@score.setter，负责把一个setter方法变成属性赋值，于是，我们就拥有一个可控的属性操作
+class Student3(object):
+
+    @property
+    def birth(self):
+        return self._birth
+
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2015 - self._birth
+#上面的birth是可读写属性，而age就是一个只读属性，因为age可以根据birth和当前时间计算出来
+
+#多重继承
+
+class Animal(object):
+    pass
+
+# 大类:
+class Mammal(Animal):
+    pass
+
+class Bird(Animal):
+    pass
+
+# 各种动物:
+class Dog(Mammal):
+    pass
+
+class Bat(Mammal):
+    pass
+
+class Parrot(Bird):
+    pass
+
+class Ostrich(Bird):
+    pass
+
+class Runnable(object):
+    def run(self):
+        print('Running...')
+
+class Flyable(object):
+    def fly(self):
+        print('Flying...')
+
+class Dog(Mammal, Runnable):
+    pass
+class Bat(Mammal, Flyable):
+    pass
+
+#Enum类
+
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+
+from enum import Enum, unique
+
+@unique
+class Weekday(Enum):
+    Sun = 0 # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+day1 = Weekday.Mon
+print(day1)
+
+class Hello(object):
+    def hello(self, name='world'):
+        print('Hello, %s.' % name)
+#解释器载入hello模块时，就会依次执行该模块的所有语句，执行结果就是动态创建出一个Hello的class对象
+from hello import Hello
+h = Hello()
+h.hello()
+
